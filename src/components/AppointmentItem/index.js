afc776aaf1177bca5af9e9d586eebf275a89f68b
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-09-30 12:24:38 +0530

    Update index.js

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -5,8 +5,16 @@ const AppointmentItem = props => {
   const {Title, date} = eachAppointment
 
   return (
-    <li>
-      <h1>{Title}</h1>
+    <li className="appointment-list">
+      <div>
+        <h1>{Title}</h1>
+        <button type="button">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png"
+            alt="star"
+          />
+        </button>
+      </div>
       <p>{date}</p>
     </li>
   )

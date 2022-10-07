2820aa82d47c59e2123dea72d78795cff042e6db
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-10-07 14:34:25 +0530

    Update index.js

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,21 +1,36 @@
 // Write your code here
 
+import {format} from 'date-fns'
+
 const AppointmentItem = props => {
-  const {eachAppointment} = props
-  const {Title, date} = eachAppointment
+  const {eachAppointment, isFavToggle} = props
+  const {Title, date, id, isFav} = eachAppointment
+
+  const onChangeFav = () => {
+    isFavToggle(id)
+  }
 
   return (
     <li className="appointment-list">
       <div>
-        <h1>{Title}</h1>
-        <button type="button">
+        <p>{Title}</p>
+        <p>{format(new Date(`${date}`), 'dd MMMM  yyyy, EEEE')}</p>
+      </div>
+      {isFav ? (
+        <button type="button" testid="star" onClick={onChangeFav}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png"
+            alt="star"
+          />
+        </button>
+      ) : (
+        <button type="button" testid="star" onClick={onChangeFav}>
           <img
             src="https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png"
             alt="star"
           />
         </button>
-      </div>
-      <p>{date}</p>
+      )}
     </li>
   )
 }
